594a2eec18fe508812d1319d3dd51942885daa61
opilane <opilane@HP-CZC6297BB7>
2020-10-12 15:04:49 +0300

    Add project files.

---
+++ vkladki/vkladki/Jaanuar.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace vkladki
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Jaanuar : ContentPage
+    {
+        public Jaanuar()
+        {
+            InitializeComponent();
+            Grid grd = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition {Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition {Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition {Height=new GridLength(1,GridUnitType.Star)}
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+                }
+        };
+            Label nimetus = new Label { Text = "Jaanuari pilt", FontSize = 30 };
+            Image img = new Image { Source = "jaanuar.jpg" };
+            Label kirjeldus = new Label { Text = "Jaanuar on esimene aasta kuu!!" };
+            grd.Children.Add(nimetus, 0, 0);
+            grd.Children.Add(img, 0, 0);
+            grd.Children.Add(kirjeldus, 0, 0);
+            Content = grd;
+        }
+    }
+}
